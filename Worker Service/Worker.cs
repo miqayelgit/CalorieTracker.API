84ffd65f06a731e019a8c8345442ad8a131935a8
miqayelgit <apple.miqayel99@gmail.com>
2026-06-22 20:04:53 +0400

    Fixed Worker Service

--- Worker Service/Worker.cs
+++ Worker Service/Worker.cs
@@ -1,18 +1,17 @@
-using CalorieTracker.Application.Contracts.Repos.UOW;
-using CalorieTracker.Application.Services.Calculations;
-using Serilog;
+﻿using CalorieTracker.Application.Contracts.Repos.UOW;
+using CalorieTracker.Application.Contracts.Services.Calculators;
 
 namespace Worker_Service
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly IServiceScopeFactory _scopeFactory;
 
-        public Worker(ILogger<Worker> logger, IUnitOfWork unitOfWork)
+        public Worker(ILogger<Worker> logger, IServiceScopeFactory scopeFactory)
         {
             _logger = logger;
-            _unitOfWork = unitOfWork;
+            _scopeFactory = scopeFactory;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -24,15 +23,60 @@ namespace Worker_Service
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
 
-                var userDatas = await _unitOfWork.ApplicationUserDataRepository.GetFromWhereAsync();
-                var calculators = new UserDataCalculators(_unitOfWork);
+                var delay = DateTime.Today.AddDays(1) - DateTime.Now;
 
-                foreach(var userData in userDatas)
+                await Task.Delay(delay, stoppingToken);
+
+                using var scope = _scopeFactory.CreateScope();
+
+                var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+                var userDataCalculators = scope.ServiceProvider.GetRequiredService<IUserDataCalculators>();
+
+                var userDatas = await unitOfWork.ApplicationUserDataRepository.GetFromWhereAsync();
+
+                foreach (var userData in userDatas)
                 {
-                    await calculators.CalculateUserDailyCalorieLimitsAsync(userData);
+                    await userDataCalculators.CalculateUserDailyCalorieLimitsAsync(userData);
                 }
-                await Task.Delay(1000, stoppingToken);
             }
         }
+
+        //THis is my implementation ->
+
+        /*     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+             {
+                 int milisecondsInDay = 86400000;
+                 var workerTriggerTimeRemaining = DateTime.Today.AddDays(1) - DateTime.Now;
+                 var milisecondsUntilMidnight = Math.Floor(workerTriggerTimeRemaining.TotalMilliseconds);
+
+                 while (!stoppingToken.IsCancellationRequested)
+                 {
+                     if (_logger.IsEnabled(LogLevel.Information))
+                     {
+                         _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                     }
+
+                     if (milisecondsUntilMidnight == 0)
+                     {
+                         using var scope = _scopeFactory.CreateScope();
+
+                         var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+                         var userDataCalculators = scope.ServiceProvider.GetRequiredService<IUserDataCalculators>();
+
+                         var userDatas = await unitOfWork.ApplicationUserDataRepository.GetFromWhereAsync();
+
+                         foreach (var userData in userDatas)
+                         {
+                             await userDataCalculators.CalculateUserDailyCalorieLimitsAsync(userData);
+                         }
+
+                         milisecondsUntilMidnight = milisecondsInDay;
+                     }
+
+
+                     await Task.Delay(TimeSpan.FromMilliseconds(milisecondsUntilMidnight), stoppingToken);
+                     milisecondsUntilMidnight = 0;
+                 }
+             }*/
     }
 }
